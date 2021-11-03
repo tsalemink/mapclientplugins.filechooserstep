374f5bd3a245cc8be269cd770b8daec3d86e7709
hsorby <h.sorby@auckland.ac.nz>
2021-11-03 21:19:23 +1300

    Single source step version. Set step location metadata.

--- mapclientplugins/filechooserstep/__init__.py
+++ mapclientplugins/filechooserstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'File Chooser'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.filechooserstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.filechooserstep import step
